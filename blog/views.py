30bb926d26de04f1cd7fa92250f5ffa8f455196c
Christian Howard <christian.john.howard@gmail.com>
2016-11-04 14:57:12 -0400

    Update views.py
    Commented for clarity

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,17 @@ from django.contrib.auth.decorators import login_required
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
+#all posts view
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+#specific post view
 def post_detail(request, pk):
     post= get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post' : post})
 
+#new post view
 @login_required
 def post_new(request):
     if request.method == 'POST':
@@ -25,6 +28,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form' : form})
 
+#edit post view
 @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -40,23 +44,27 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+#unpublished posts view
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+#publish posts
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('blog.views.post_detail', pk=pk)
 
+#remove post
 @login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('blog.views.post_list')
 
+#add comment to post
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -70,12 +78,14 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
+#approve comment
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('blog.views.post_detail', pk=comment.post.pk)
 
+#remove comment
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
