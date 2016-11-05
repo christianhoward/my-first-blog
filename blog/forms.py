b875f3cda90ce615c9a1b48fa39fffbc297b0e61
Christian Howard <christian.john.howard@gmail.com>
2016-11-05 18:15:24 -0400

    Update forms.py
    Commented for clarity

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from .models import Post, Comment
 
+#form for posts
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
-        
+
+#form for comments
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
